6903145f38d625cdaabfb44f211c9879136d4c9d
Nora Zheng <zhengnuo@gmail.com>
2019-10-07 02:24:27 -0400

    DisplayData.js now displays the csv as a table

--- src/Components/DisplayData.js
+++ src/Components/DisplayData.js
@@ -3,10 +3,51 @@ import React, { Component } from 'react';
 class DisplayData extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			data: [],
+			headers: []
+		};
 	}
+
+	handleData = data => {
+		//the first element is an array of headers like id, names
+		const headers = data.shift();
+		this.setState({ data: data, headers: headers });
+	};
+
+	componentDidMount = () => {
+		this.handleData(this.props.data);
+	};
+
 	render() {
-		return <p>data here</p>;
+		return (
+			<table>
+				<thead>
+					<tr>
+						{this.state.headers.map((header, index) => {
+							return (
+								<th id={`header${header}`} key={`header${index}`}>
+									{header}
+								</th>
+							);
+						})}
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.data.map(row => {
+						return (
+							<tr>
+								{row.map((value, index) => {
+									return (
+										<td className={`${this.state.headers[index]}`}>{value}</td>
+									);
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		);
 	}
 }
 
