9bce3b7cfd493489f83b8fe2cf4ba23bc4de5099
Nora Zheng <zhengnuo@gmail.com>
2019-10-07 04:35:15 -0400

    app styling

--- src/Components/DisplayData.js
+++ src/Components/DisplayData.js
@@ -14,19 +14,32 @@ class DisplayData extends Component {
 	}
 
 	calcMaxPage = () => {
+		console.log(
+			Math.ceil((this.state.data.length - 1) / this.state.rowsPerPage)
+		);
 		//calculating the max page number
 		return Math.ceil((this.state.data.length - 1) / this.state.rowsPerPage);
 	};
 
 	handleData = data => {
-		//the first element is an array of headers like id, names
+		//the first element of data array is an array of headers like id, names
 		const headers = data.shift();
-		this.setState({ data: data, headers: headers }, () => {
-			this.getCurrentPage(this.state.pageNum);
+
+		// clean up dirty data, filter out rows with missing data
+		const cleanData = this.props.data.filter(row => {
+			return row.length === headers.length;
+		});
+
+		this.setState({ data: cleanData, headers: headers }, () => {
+			if (this.state.pageNum < this.calcMaxPage()) {
+				this.getPage(this.state.pageNum);
+			} else {
+				this.getPage(this.calcMaxPage());
+			}
 		});
 	};
 
-	getCurrentPage = pageNum => {
+	getPage = pageNum => {
 		//pagination based on the current page number and rows to show per page
 		//get a slice of the csv data
 		const currentPage = this.state.data.slice(
@@ -36,15 +49,51 @@ class DisplayData extends Component {
 		this.setState({ currentPage: currentPage, pageNum: pageNum });
 	};
 
-	updateRowsPerPage = e => {
+	sortData = (column, ascending) => {
+		const tbody = document.getElementById('tbody');
+		const rows = tbody.rows;
+		let unsorted = true;
+		// loop until no sorting to be done
+
+		while (unsorted) {
+			//start by saying: no switching is done:
+			unsorted = false;
+
+			/*Loop through all table rows (except the
+    first, which contains table headers):*/
+			for (let r = 0; r < rows.length - 1; r++) {
+				//start by saying there should be no switching:
+
+				/*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+				const v1 = rows[r].querySelector(`.${column}`).innerHTML;
+				const v2 = rows[r + 1].querySelector(`.${column}`).innerHTML;
+
+				//check if the two rows should switch place:
+				if (
+					ascending
+						? !isNaN(parseFloat(v1)) && !isNaN(parseFloat(v2))
+							? parseFloat(v1) > parseFloat(v2)
+							: v1 > v2
+						: !isNaN(parseFloat(v1)) && !isNaN(parseFloat(v2))
+						? parseFloat(v1) < parseFloat(v2)
+						: v1 < v2
+				) {
+					tbody.insertBefore(rows[r + 1], rows[r]);
+					unsorted = true;
+				}
+			}
+		}
+	};
+	selectRowsPerPage = e => {
 		// show only certain number of rows based on user's preference
 		let rowsPerPage = e.target.value;
 		this.setState({ rowsPerPage: rowsPerPage }, () => {
 			// if current page number exceeds the new max page number, go to the new max page
 			if (this.state.pageNum < this.calcMaxPage()) {
-				this.getCurrentPage(this.state.pageNum);
+				this.getPage(this.state.pageNum);
 			} else {
-				this.getCurrentPage(this.calcMaxPage());
+				this.getPage(this.calcMaxPage());
 			}
 		});
 	};
@@ -53,29 +102,37 @@ class DisplayData extends Component {
 		this.handleData(this.props.data);
 	};
 
+	componentDidUpdate(prevProps) {
+		//when new csv file is uploaded
+		if (this.props.data !== prevProps.data) {
+			this.handleData(this.props.data);
+		}
+	}
+
 	render() {
 		return (
 			<section className='data'>
 				<label>
 					Show
-					<select id='rowsPerPage' onChange={this.updateRowsPerPage}>
+					<select id='rowsPerPage' onChange={this.selectRowsPerPage}>
 						<option value='20'>20</option>
 						<option value='50'>50</option>
 						<option value='100'>100</option>
 						<option value='200'>200</option>
+						{/* max set to 200 for sorting performance */}
 					</select>
 					rows per page
 				</label>
 				{/* if already at first page, disable prev button */}
 				{this.state.pageNum === 1 ? (
-					<button className='prevPage' disabled>
+					<button className='nav' disabled>
 						&lt; <span className='visuallyHidden'>previous page</span>
 					</button>
 				) : (
 					<button
-						className='prevPage'
+						className='nav'
 						onClick={() => {
-							this.getCurrentPage(this.state.pageNum - 1);
+							this.getPage(this.state.pageNum - 1);
 						}}>
 						&lt; <span className='visuallyHidden'>previous page</span>
 					</button>
@@ -87,47 +144,67 @@ class DisplayData extends Component {
 				</label>
 
 				{this.state.pageNum === this.calcMaxPage() ? (
-					<button className='nextPage' disabled>
+					<button className='nav' disabled>
 						&gt; <span className='visuallyHidden'>next page</span>
 					</button>
 				) : (
 					<button
-						className='prevPage'
+						className='nav'
 						onClick={() => {
-							this.getCurrentPage(this.state.pageNum + 1);
+							this.getPage(this.state.pageNum + 1);
 						}}>
 						&gt; <span className='visuallyHidden'>next page</span>
 					</button>
 				)}
-
-				<table>
-					<thead>
-						<tr>
-							{this.state.headers.map((header, index) => {
+				<div className='scroll'>
+					<table>
+						<thead>
+							<tr>
+								{this.state.headers.map((header, index) => {
+									return (
+										<th
+											className='header'
+											id={`header${header}`}
+											key={`header${index}`}>
+											{header}
+											<div className='buttons'>
+												<button
+													onClick={() => {
+														this.sortData(header, true);
+													}}
+													className='sort'>
+													<span className='visuallyHidden'>Ascending</span>▲
+												</button>
+												<button
+													onClick={() => {
+														this.sortData(header, false);
+													}}
+													className='sort'>
+													<span className='visuallyHidden'>Descending</span>▼
+												</button>
+											</div>
+										</th>
+									);
+								})}
+							</tr>
+						</thead>
+						<tbody id='tbody'>
+							{this.state.currentPage.map(row => {
 								return (
-									<th id={`header${header}`} key={`header${index}`}>
-										{header}
-									</th>
+									<tr>
+										{row.map((value, index) => {
+											return (
+												<td className={`${this.state.headers[index]}`}>
+													{value}
+												</td>
+											);
+										})}
+									</tr>
 								);
 							})}
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.currentPage.map(row => {
-							return (
-								<tr>
-									{row.map((value, index) => {
-										return (
-											<td className={`${this.state.headers[index]}`}>
-												{value}
-											</td>
-										);
-									})}
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
+						</tbody>
+					</table>
+				</div>
 			</section>
 		);
 	}
