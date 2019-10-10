4aad0c054901717122fb719eb5ce02ddacd6fb3e
Nora <zhengnuo@gmail.com>
2019-10-10 05:05:01 -0400

    Updates

--- precache-manifest.10c6752cabc70baf3768f5cbac3317a0.js
+++ precache-manifest.10c6752cabc70baf3768f5cbac3317a0.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cbed348de022be43faa84b2a9f527309",
+    "revision": "ce5d0c5ed6a38d60f2d14a53e65cb3be",
     "url": "/react-csv-reader/index.html"
   },
   {
-    "revision": "4c0fc2d4c9a45f5f2628",
-    "url": "/react-csv-reader/static/css/main.44fcea42.chunk.css"
+    "revision": "5e3e24f2c343546b9498",
+    "url": "/react-csv-reader/static/css/main.cc1d5911.chunk.css"
   },
   {
     "revision": "2ec3eb2f7c5e88fa0bfb",
     "url": "/react-csv-reader/static/js/2.054f0375.chunk.js"
   },
   {
-    "revision": "4c0fc2d4c9a45f5f2628",
-    "url": "/react-csv-reader/static/js/main.60861c14.chunk.js"
+    "revision": "5e3e24f2c343546b9498",
+    "url": "/react-csv-reader/static/js/main.608bbace.chunk.js"
   },
   {
     "revision": "c9b2b29f93e65ee669d8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-csv-reader/precache-manifest.7f1851f98a2021c6f0bac85330e06d45.js"
+  "/react-csv-reader/precache-manifest.10c6752cabc70baf3768f5cbac3317a0.js"
 );
 
 self.addEventListener('message', (event) => {
